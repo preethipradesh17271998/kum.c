32abe4f81b4aeebfe0637d58d7908f1c5bbda0e0
preethipradesh17271998 <31505560+preethipradesh17271998@users.noreply.github.com>
2018-03-04 16:48:48 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -2,10 +2,10 @@
 #include<conio.h>
 void main()
 {
-char ch:
+char ch;
 printf("enter the alphabet");
 scanf("%c",&ch);
-if(ch=='a'!!ch=='e'!!ch=='i'!!ch=='o'!!ch=='u'!!ch=='A'!!ch=='E'!!ch=='I'!!ch=='O'!!ch=='U')
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
 {
 printf("entered character is vowel");
 }
