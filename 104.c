f0e7c704dd2a0a8ac9ddc73141a2267d45263a0a
preethipradesh17271998 <31505560+preethipradesh17271998@users.noreply.github.com>
2018-03-12 15:50:40 +0530

    Create 104.c

---
+++ 104.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int n,k,c;
+	printf("enter the number");
+	scanf("%d",&n);
+	printf("enter the number");
+	scanf("%d",&k);
+	c=pow(n,k);
+	printf("the value is %d",c);
+	return 0;
+}
