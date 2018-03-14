828ade37509b5b7372a9b6f429e908cab7751f5f
preethipradesh17271998 <31505560+preethipradesh17271998@users.noreply.github.com>
2018-03-14 17:00:03 +0530

    Create 113w.c

---
+++ 113w.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int n,k,a[10],i,c=0;
+	printf("\n enter the values of n and k ");
+	scanf("%d%d",&n,&k);
+	printf("\n enter the values ");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		if(a[i]==k)
+		{
+			c++;
+		}
+		
+	}
+	printf("%d",c);
+		return 0;
+	
+}
